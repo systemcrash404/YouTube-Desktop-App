3739e708664406ecced0fdcc6a42420acdba04c8
systemcrash404 <73080127+systemcrash404@users.noreply.github.com>
2022-06-02 19:46:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + '/assets/icon.ico',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+
+    }
+  })
+
+  mainWindow.loadURL('https://www.youtube.com')
+
+
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+
+
+    
+
+    
+
